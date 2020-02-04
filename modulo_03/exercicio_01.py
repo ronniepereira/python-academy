eaa8f790e51f86a25c20f8fce23a88eeb27beacc
Ronnie Pereira <vinniciusrp@gmail.com>
2020-02-04 21:52:40 -0200

    first commit

---
+++ modulo_03/exercicio_01.py
@@ -0,0 +1,9 @@
+
+nota_01 = float(input("Digite a primeira nota: "))
+nota_02 = float(input("Digite a segunda nota: "))
+nota_03 = float(input("Digite a terceira nota: "))
+nota_04 = float(input("Digite a quarta nota: "))
+
+media = (nota_01 + nota_02 + nota_03 + nota_04) / 4
+
+print(media)
